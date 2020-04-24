8e2cd2e35b464b91c7aed4663c32e99360ffcdb1
DELL PRECISION M4800 <ltp7509@gmail.com>
2020-04-24 17:42:20 +0700

    lession10

---
+++ TeduCoreApp.Data/Entities/ProductCategory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TeduCoreApp.Data.Enums;
+using TeduCoreApp.Data.Interfaces;
+using TeduCoreApp.Infrastructure.SharedKernel;
+
+namespace TeduCoreApp.Data.Entities
+{
+    public class ProductCategory : DomainEntity<int>, IHasSeoMetaData, ISortable, ISwitchable, IDateTracking
+    {
+        public ProductCategory()
+        {
+            Products = new List<Product>();
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ParentId { get; set; }
+        public int? HomeOrder { get; set; }
+        public string Image { get; set; }
+        public bool? HomeFlag { get; set; }
+        public int SortOrder { set; get; }
+        public DateTime DateCreated { set; get; }
+        public DateTime DateModified { set; get; }
+        public Status Status { set; get; }
+        public string SeoPageTitle { set; get; }
+        public string SeoAlias { set; get; }
+        public string SeoKeywords { set; get; }
+        public string SeoDescription { set; get; }
+
+        public virtual ICollection<Product> Products { set; get; }
+    }
+}
---
+++ TeduCoreApp.Data/Entities/ProductTag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TeduCoreApp.Infrastructure.SharedKernel;
+
+namespace TeduCoreApp.Data.Entities
+{
+    public class ProductTag : DomainEntity<int>
+    {
+        public int ProductId { set; get; }
+
+        [StringLength(50)]
+        [Column(TypeName = "varchar")]
+        public string TagId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { set; get; }
+
+        [ForeignKey("TagId")]
+        public virtual Tag Tag { set; get; }
+    }
+}
